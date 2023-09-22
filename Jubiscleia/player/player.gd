7032428748ee51f11037e07847af4eceec854efc
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2023-09-22 09:46:24 -0300

    Ajustes no pulo
    Agora o pulo pode ser definido melhor

--- Jubiscleia/player/player.gd
+++ Jubiscleia/player/player.gd
@@ -1,18 +1,28 @@
 extends CharacterBody2D
 
-@onready var jump_cd: Timer = $JumpCD
-
 @export var speed: float = 300.0
-@export var jump_force: float = -400.0
-@export var jump_count: float
-@export var max_jump_count: float
 
-@export var can_jump: bool
+@export_group("Jump Variables")
+@export var jump_height: float
+@export var jump_time_to_peak: float
+@export var jump_time_to_descent: float
+var first_jump: bool
+var double_jump: bool
+
+var jump_velocity: float
+var jump_gravity: float
+var fall_gravity: float
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+func _ready():
+	jump_velocity = ((2.0 * jump_height) / jump_time_to_peak) * -1
+	jump_gravity = ((-2.0 * jump_height) / pow(jump_time_to_peak,2)) * -1
+	fall_gravity = ((-2.0 * jump_height) / pow(jump_time_to_descent,2)) * -1
+	
 func _physics_process(delta):
+	get_gravity()
 	
 	move_player(delta)
 	
@@ -23,17 +33,23 @@ func move_player(delta) -> void:
 	
 	if not is_on_floor():  # Faz o player ser afetado pela gravidade
 		velocity.y += gravity * delta
+		first_jump = false
 	else:
-		can_jump = true
-		jump_count = 0
+		first_jump = true
+		double_jump = true
 	
-	#if Input.is_action_just_pressed("jump") and jump_count < max_jump_count: # Faz o player pular
-	#	velocity.y = jump_force
-	#	jump_count += 1
-	
-	if Input.is_action_pressed("jump") and jump_count < max_jump_count:
-		velocity.y = jump_force
-		jump_count += 1
+	if Input.is_action_just_pressed("jump"):
+		if first_jump:
+			velocity.y = jump_velocity
+			first_jump = false
+		elif double_jump:
+			velocity.y = jump_velocity
+			double_jump = false
 	
 	velocity.x = direction * speed
 	
+func get_gravity():
+	if velocity.y < 0:
+		gravity = jump_gravity
+	else:
+		gravity = fall_gravity
