012965c44815f6a0e402d038fd63b0d99c39972f
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2023-10-04 14:11:44 -0300

    Testes camera offset

--- Jubiscleia/player/Camera.gd
+++ Jubiscleia/player/Camera.gd
@@ -1,5 +1,13 @@
 extends Camera2D
 
+@export var player: CharacterBody2D
+@export var camera_offset_x: float
+@export var camera_offset_y: float
+
+func _process(_delta):
+	if player.is_on_floor():
+		pass
+
 func change_limit(top_limit: int, left_limit: int, right_limit: int, bottom_limit: int) -> void:
 	limit_top = top_limit
 	limit_left = left_limit
