d70f413ff2539be7a4347856e62cdda69a9fa171
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2023-11-06 08:23:50 -0300

    Mais modificacoes no ataque
    Se o jogador continuar atacando ele nao anda no fim do ataque

--- Jubiscleia/Management/states/player/attacks/sword_attack_1.gd
+++ Jubiscleia/Management/states/player/attacks/sword_attack_1.gd
@@ -4,6 +4,7 @@ extends State
 @export var player: CharacterBody2D
 @export var animation: AnimationPlayer
 var can_cancel: bool
+var can_move: bool
 
 func _ready():
 	set_physics_process(false)
@@ -12,6 +13,7 @@ func enter_state() -> void:
 	set_physics_process(true)
 	animation.play("sword_attack_1")
 	can_cancel = false
+	can_move = true
 
 func exit_state() -> void:
 	set_physics_process(false)
@@ -20,10 +22,13 @@ func _physics_process(_delta):
 	player.velocity.x = 0
 	player.direction = Input.get_axis("left","right")
 	
-	if Input.is_action_just_pressed("basic_Attack") and can_cancel:
-		player.fsm.change_state(player.sword_attack_2_state)
+	if Input.is_action_just_pressed("basic_Attack"):
+		if can_cancel:
+			player.fsm.change_state(player.sword_attack_2_state)
+		else:
+			can_move = false
 	
-	if player.direction != 0 and can_cancel:
+	if player.direction != 0 and can_move and can_cancel:
 		player.fsm.change_state(player.move_state)
 	
 	if player.health_component.is_getting_hit:
--- Jubiscleia/Management/states/player/attacks/sword_attack_2.gd
+++ Jubiscleia/Management/states/player/attacks/sword_attack_2.gd
@@ -3,6 +3,7 @@ extends State
 
 @export var player: CharacterBody2D
 @export var animation: AnimationPlayer
+var can_cancel: bool
 
 func _ready():
 	set_physics_process(false)
@@ -10,6 +11,7 @@ func _ready():
 func enter_state() -> void:
 	set_physics_process(true)
 	animation.play("sword_attack_2")
+	can_cancel = false
 
 func exit_state() -> void:
 	set_physics_process(false)
@@ -20,9 +22,15 @@ func _physics_process(_delta):
 	if player.health_component.is_getting_hit:
 		player.fsm.change_state(player.hit_state)
 	
+	if player.direction != 0 and can_cancel:
+		player.fsm.change_state(player.move_state)
+	
 	if not player.alive:
 		player.fsm.change_state(player.death_state)
 
 func _on_animation_finished(anim):
 	if anim == "sword_attack_2":
 		player.fsm.change_state(player.idle_state)
+
+func can_cancel_true():
+	can_cancel = true
