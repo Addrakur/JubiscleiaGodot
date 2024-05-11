ea9307ee334a9ca74ebe35d8a42b72721fd6399b
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2024-05-11 17:13:15 -0300

    Começo implementação Kitsune

--- Jubiscleia/Management/states/big_worm/big_worm_death.gd
+++ Jubiscleia/Management/states/big_worm/big_worm_death.gd
@@ -9,14 +9,12 @@ func _ready():
 
 func enter_state() -> void:
 	set_physics_process(true)
-	print("enter")
 	big_worm.health_component.is_getting_hit = false
 	animation.play("death")
 	big_worm.limit.erase(big_worm)
 
 func exit_state() -> void:
 	set_physics_process(false)
-	print("exit")
 
 func _physics_process(_delta):
 	big_worm.velocity.x = 0
