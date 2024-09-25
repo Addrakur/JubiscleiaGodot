5281ac1300eedc02c723a0a5eccc4b806b85ad20
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2024-09-25 19:26:26 -0300

    Ajustes no sistema de arenas

--- Jubiscleia/arena_manager.gd
+++ Jubiscleia/arena_manager.gd
@@ -12,20 +12,21 @@ extends Node2D
 
 @onready var wave_interval: Timer = $WaveInterval
 
-var current_wave: int = 0
+var current_wave: int = 1
 
 func _ready() -> void:
 	close_region.body_entered.connect(start_arena.bind())
 
 
 func wave_start():
-	var new_wave: Wave = waves[current_wave]
+	var new_wave: Wave = waves[current_wave - 1] #Arrumar isso aqui, não sei o que está dando errado
 	new_wave.spawn_enemies()
 	new_wave.active = true
 	
 
 func next_wave():
-	if current_wave == max_waves - 1:
+	if current_wave == max_waves:
+		arena_finish()
 		return
 	current_wave += 1
 	wave_interval.start(interval_time)
@@ -37,9 +38,17 @@ func start_arena(body: CharacterBody2D):
 	if not body.is_in_group("player"):
 		return
 	
+	wall_manager.create_entrance_wall()
 	wave_interval.start(interval_time)
 	close_region.queue_free()
 
 func _process(delta: float) -> void:
 	if wave_interval.time_left != 0:
+		interval_label.visible = true
 		interval_label.text = str(int(wave_interval.time_left))
+	else:
+		interval_label.visible = false
+
+func arena_finish():
+	wall_manager.destroy_walls()
+	interval_label.visible = false
--- Jubiscleia/enemy_kill_wall_manager.gd
+++ Jubiscleia/enemy_kill_wall_manager.gd
@@ -1,21 +1,29 @@
 class_name EnemyKillWallManager
 extends Node2D
 
-@export var enemies: Array[CharacterBody2D]
-@export var wall_folder: Node2D
-var walls: Array[StaticBody2D]
+@export var exit_walls: Node2D
+@export var entrance_walls: Node2D
+
+var all_entrance_walls: Array
+var all_exit_walls: Array
 
 func _ready() -> void:
-	var wall_temp = wall_folder.get_children()
-	for child in wall_temp:
-		walls.append(child)
+	all_entrance_walls = entrance_walls.get_children()
+	all_exit_walls = exit_walls.get_children()
+	for wall in all_entrance_walls:
+		wall.set_collision_layer_value(17,false)
+	entrance_walls.visible = false
 
-func _process(_delta: float) -> void:
-	
-	for enemy in enemies:
-		if enemy != null:
-			return
-	
-	for wall in walls:
+func destroy_walls():
+	for wall in all_exit_walls:
 		if wall != null:
 			wall.queue_free()
+	
+	for wall in all_entrance_walls:
+		if wall != null:
+			wall.queue_free()
+
+func create_entrance_wall():
+	entrance_walls.visible = true
+	for wall in all_entrance_walls:
+		wall.set_collision_layer_value(17,true)
