db949f5d5383c86b4a8db0b1feeb839a92cbbb8f
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2025-03-28 17:18:06 -0300

    Shader para o efeito de slow colocado no dummy

--- Jubiscleia/Management/composition_scenes/slow_timer.gd
+++ Jubiscleia/Management/composition_scenes/slow_timer.gd
@@ -3,8 +3,13 @@ extends Timer
 
 var parent: Node2D
 
+var material: ShaderMaterial
+
 func _ready() -> void:
 	parent = get_parent()
+	material = parent.texture.get_material()
+	material.set_shader_parameter("blue_value",0.4)
 
 func _on_timeout() -> void:
 	parent.speed = 1
+	material.set_shader_parameter("blue_value",0.0)
