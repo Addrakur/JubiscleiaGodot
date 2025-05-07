2213f83757f282ed1900db4376c0f567044b6792
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2025-05-07 10:16:34 -0300

    Mudanças no tutorial
    Mudanças nas placas e no orbe elemental, agora o orbe elemental não vai ser absorvido automaticamente, o jogador precisa apertar um botão.

--- Jubiscleia/Management/elemental_orb.gd
+++ Jubiscleia/Management/elemental_orb.gd
@@ -4,11 +4,27 @@ extends Node2D
 @onready var timer: Timer = $timer
 @onready var texture: Sprite2D = $texture
 @export var animation: AnimationPlayer
+@onready var botao_e: Sprite2D = $BotaoE
+
+var can_collect: bool = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
-		GameSettings.player.element_manager.rupture(element)
-		queue_free()
+	if body is Player:
+		botao_e.visible = true
+		can_collect = true
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Player:
+		botao_e.visible = false
+		can_collect = false
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("collect_orb") and can_collect:
+		collect_orb()
 
 func set_animation():
 	animation.play(element)
+
+func collect_orb():
+	GameSettings.player.element_manager.rupture(element)
+	queue_free()
