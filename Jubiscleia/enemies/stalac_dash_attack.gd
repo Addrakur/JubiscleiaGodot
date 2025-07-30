9e8f1bd0ab68d350ecbc3bd02887aec877274ed6
Addrakur <roberto.antonio.pocai.filho@gmail.com>
2025-07-30 10:18:58 -0300

    Dash attack do stalac funcionando

--- Jubiscleia/enemies/stalac_dash_attack.gd
+++ Jubiscleia/enemies/stalac_dash_attack.gd
@@ -3,12 +3,14 @@ extends LimboState
 @export var parent: Stalac
 @export var animation: AnimationPlayer
 @export var attack_timer: Timer
+@export var dash_speed: float
 
 var can_rotate: bool = false
+var can_move: bool = false
 
 func _enter():
 	parent.velocity.x = 0
-	#animation.play("attack")
+	animation.play("attack")
 	set_direction()
 	print("attack")
 
@@ -16,16 +18,20 @@ func _update(delta) -> void:
 	if can_rotate:
 		rotate_to_target(delta)
 	
+	if can_move:
+		parent.velocity.x = dash_speed * parent.direction * delta
+	
 	if parent.direction == 1 and parent.texture.rotation_degrees >= 90 or parent.direction == -1 and parent.texture.rotation_degrees <= -90:
 		can_rotate = false
 
 func _exit():
 	parent.can_attack = false
 	print("exit attack")
-	#attack_timer.start()
+	parent.texture.rotation_degrees = 0
+	attack_timer.start()
 
 func rotate_to_target(delta: float):
-	parent.texture.rotation_degrees += 1 * parent.direction
+	parent.texture.rotation_degrees += 200 * parent.direction * delta
 
 func set_direction():
 	if parent.player_ref.position.x > parent.position.x:
@@ -36,5 +42,8 @@ func set_direction():
 
 func _on_animation_animation_finished(anim_name: StringName) -> void:
 	pass
-	#if anim_name == "attack":
-		#dispatch("attack_to_idle")
+	if anim_name == "attack":
+		dispatch("attack_to_idle")
+
+func can_move_toggle(t_or_f: bool):
+	can_move = t_or_f
